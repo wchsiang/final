6ed2af282b4a122c66bfb99eeb92b7742761edc1
MilkyCat <opopopop50226@gmail.com>
2024-12-15 17:00:23 +0800

    pwd

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = getenv('DB_Pwd') ?: "";
 $dbname = "dwp_final";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
